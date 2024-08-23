0a2503ca4a6ce8855d5f44414f4dc95acb9e9e44
Dark-LYNN <botdevlynn@gmailcom>
2024-08-23 17:54:15 +0200

    Commands crashed if it got anything other than a get

--- src/routes/commands.ts
+++ src/routes/commands.ts
@@ -5,10 +5,16 @@ export const data = {
     method: "get",
     auth: true,
     handler: async function (ctx) {
-        ctx.reply.end(JSON.stringify(ForgeAPI.client.commands.toArray().map(s=>{return s.data})))
+        if (ctx.request.method !== 'GET') {
+            ctx.reply.writeHead(405, { 'Content-Type': 'application/json' });
+            ctx.reply.end(JSON.stringify({ status: 405, message: 'Method Not Allowed' }));
+            return;
+        }
+
+        ctx.reply.end(JSON.stringify(ForgeAPI.client.commands.toArray().map(s => { return s.data; })));
     },
     wsHandler: async function(ctx) {
-        ctx.ws.send(JSON.stringify(ForgeAPI.client.commands.toArray().map(s=>{return s.data})))
-        ctx.client.commands.on('update', () => ctx.ws.send(JSON.stringify(ForgeAPI.client.commands.toArray().map(s=>{return s.data}))))
+        ctx.ws.send(JSON.stringify(ForgeAPI.client.commands.toArray().map(s => { return s.data; })));
+        ctx.client.commands.on('update', () => ctx.ws.send(JSON.stringify(ForgeAPI.client.commands.toArray().map(s => { return s.data; }))));
     }
 } as RouteOptions
